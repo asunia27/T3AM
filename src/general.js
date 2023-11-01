b7935893ac45209f840e1c3a03d0cf1b05135f49
Eric F Bau <33136802+efbau@users.noreply.github.com>
2023-10-31 19:01:09 -0500

    Documentation for general.js

--- src/general.js
+++ src/general.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+{/*
+    A collection of small, generic components that may be reused.
+
+    Current Listing:
+        DropdownSelect
+*/}
+
+{/* Dropdown menu. Self explanatory */}
 export default function DropdownSelect({options}){
     const listItems = options.map(item => 
         <option>{item}</option>
