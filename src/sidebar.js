7d6c256a0029d653234be018902bf6ce3e4e30d9
Blufyrein <blufyrein@gmail.com>
2023-11-12 00:14:04 -0600

    fix routing 3

--- src/sidebar.js
+++ src/sidebar.js
@@ -6,10 +6,10 @@ export default props => {
     return (
         <Menu height={'100%'}>
             <div className='sidebar-title'> Menu </div>
-            <div><NavLink to='/' className='bm-link'>Home</NavLink></div>
-            <div><NavLink to='/timeline' className='bm-link'>Edit Timeline</NavLink></div>
-            <div><NavLink to='/about' className='bm-link'>Medical School Information</NavLink></div>
-            <div><NavLink to='/contact' className='bm-link'>Ask a Professional</NavLink></div>
+            <div><NavLink to='/T3AM/' className='bm-link'>Home</NavLink></div>
+            <div><NavLink to='/T3AM/timeline' className='bm-link'>Edit Timeline</NavLink></div>
+            <div><NavLink to='/T3AM/about' className='bm-link'>Medical School Information</NavLink></div>
+            <div><NavLink to='/T3AM/contact' className='bm-link'>Ask a Professional</NavLink></div>
         </Menu>
     );
 };
