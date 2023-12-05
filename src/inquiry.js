24ebc95470a1743a7187723b6033e0d241521771
Djeggis <Andrew.William.Gerungan@gmail.com>
2023-12-05 11:30:02 -0600

    added error prevention in ask a pro
    -made users unable to send inquiry when invalid dropdown and empty text field
    
    -text field empties on send

--- src/inquiry.js
+++ src/inquiry.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Notif from './notif';
 import DropdownSelect from './general';
 
 export default function Inquiry() {
-    const field = [
+    const fieldList = [
         'Neuroscience',
         'Nursing',
         'Biomedical',
@@ -11,13 +11,38 @@ export default function Inquiry() {
         'Other'
     ];
     
-    const topic = [
+    const topicList = [
         'After College',
         'Medical School',
         'Clinical Experience',
         'Recommendation Letters',
         'Other'
     ];
+
+    const [field, setField] = useState("");
+    const [topic, setTopic] = useState("");
+    const [inq, setInq] = useState("");
+
+    const sendInquiry = () => {
+        let valid = true;
+        if(!fieldList.includes(field) || !topicList.includes(topic)){
+            if(!fieldList.includes(field)){
+                Notif('Semester field is not filled in!', 'error', '/');
+            }
+            if(!topicList.includes(topic)){
+                Notif('Year field is not filled in!', 'error', '/');
+            }
+            valid = false;
+        }
+        if(inq === ""){
+            Notif('Inquiry is blank!', 'error', '/');
+            valid = false;
+        }
+        if(valid){
+            Notif('Inquiry sent successfully!', 'success', '/');
+            setInq("");
+        }
+    }
 
     return(
         <div>
@@ -27,19 +52,17 @@ export default function Inquiry() {
             </div>
                 <div className='d-flex justify-content-between row mb-5'>
                 <div className='col-5'>
-                    <DropdownSelect fnc={'-'} placeholder={'Field Type'} options={field}/>
+                    <DropdownSelect fnc={setField} placeholder={'Field Type'} options={fieldList}/>
                 </div>
                 <div className='col-5'>
-                    <DropdownSelect fnc={'-'} placeholder={'Inquiry Type'} options={topic}/>
+                    <DropdownSelect fnc={setTopic} placeholder={'Inquiry Type'} options={topicList}/>
                 </div>
             </div>
             <div className='mb-5'>
-                <textarea name='inquiryInput' className='form-control' placeholder='Inquiry begins here...' style={{minHeight: '15rem'}}></textarea>
+                <textarea value = {inq} onChange={ (e) => setInq(e.target.value)} name='inquiryInput' className='form-control' placeholder='Inquiry begins here...' style={{minHeight: '15rem'}}></textarea>
             </div>
             <div className='mb-5'>
-                <button onClick={
-                    () => Notif('Inquiry sent successfully!', 'success', '/')
-                } className='btn btn-primary btn-lg px-5' style={{backgroundColor: '#2b85a1'}}>Send</button>
+                <button onClick={ () => sendInquiry() } className='btn btn-primary btn-lg px-5' style={{backgroundColor: '#2b85a1'}}>Send</button>
             </div>  
         </div>
     );
